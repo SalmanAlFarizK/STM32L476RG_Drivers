2adc39f52f286e3a2cc9ede7897dcacbf275ad70
SalmanAlFarizK <alfarizksalman@gmail.com>
2025-09-06 00:52:38 +0530

    Commit Contains the following: 1)  User LED blinking one User button press. 2) UART2 TX and RX driver.

---
+++ 001_GPIO_IP_OP/Debug/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_dma_ex.d
@@ -0,0 +1,54 @@
+Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_dma_ex.o: \
+ ../Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_dma_ex.c \
+ D:/BITSILICA/SALMAN/Learning/STM32/STM32_Projects/STM32L476RG_Drivers/001_GPIO_IP_OP/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal.h \
+ ../Inc/stm32l4xx_hal_conf.h \
+ D:/BITSILICA/SALMAN/Learning/STM32/STM32_Projects/STM32L476RG_Drivers/001_GPIO_IP_OP/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_rcc.h \
+ D:/BITSILICA/SALMAN/Learning/STM32/STM32_Projects/STM32L476RG_Drivers/001_GPIO_IP_OP/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_def.h \
+ D:/BITSILICA/SALMAN/Learning/STM32/STM32_Projects/STM32L476RG_Drivers/001_GPIO_IP_OP/Drivers/CMSIS/Device/ST/STM32L4xx/Include/stm32l4xx.h \
+ D:/BITSILICA/SALMAN/Learning/STM32/STM32_Projects/STM32L476RG_Drivers/001_GPIO_IP_OP/Drivers/CMSIS/Device/ST/STM32L4xx/Include/stm32l476xx.h \
+ D:/BITSILICA/SALMAN/Learning/STM32/STM32_Projects/STM32L476RG_Drivers/001_GPIO_IP_OP/Drivers/CMSIS/Include/core_cm4.h \
+ D:/BITSILICA/SALMAN/Learning/STM32/STM32_Projects/STM32L476RG_Drivers/001_GPIO_IP_OP/Drivers/CMSIS/Include/cmsis_version.h \
+ D:/BITSILICA/SALMAN/Learning/STM32/STM32_Projects/STM32L476RG_Drivers/001_GPIO_IP_OP/Drivers/CMSIS/Include/cmsis_compiler.h \
+ D:/BITSILICA/SALMAN/Learning/STM32/STM32_Projects/STM32L476RG_Drivers/001_GPIO_IP_OP/Drivers/CMSIS/Include/cmsis_gcc.h \
+ D:/BITSILICA/SALMAN/Learning/STM32/STM32_Projects/STM32L476RG_Drivers/001_GPIO_IP_OP/Drivers/CMSIS/Include/mpu_armv7.h \
+ D:/BITSILICA/SALMAN/Learning/STM32/STM32_Projects/STM32L476RG_Drivers/001_GPIO_IP_OP/Drivers/CMSIS/Device/ST/STM32L4xx/Include/system_stm32l4xx.h \
+ D:/BITSILICA/SALMAN/Learning/STM32/STM32_Projects/STM32L476RG_Drivers/001_GPIO_IP_OP/Drivers/STM32L4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ D:/BITSILICA/SALMAN/Learning/STM32/STM32_Projects/STM32L476RG_Drivers/001_GPIO_IP_OP/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_rcc_ex.h \
+ D:/BITSILICA/SALMAN/Learning/STM32/STM32_Projects/STM32L476RG_Drivers/001_GPIO_IP_OP/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_gpio.h \
+ D:/BITSILICA/SALMAN/Learning/STM32/STM32_Projects/STM32L476RG_Drivers/001_GPIO_IP_OP/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_gpio_ex.h \
+ D:/BITSILICA/SALMAN/Learning/STM32/STM32_Projects/STM32L476RG_Drivers/001_GPIO_IP_OP/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_dma.h \
+ D:/BITSILICA/SALMAN/Learning/STM32/STM32_Projects/STM32L476RG_Drivers/001_GPIO_IP_OP/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_cortex.h \
+ D:/BITSILICA/SALMAN/Learning/STM32/STM32_Projects/STM32L476RG_Drivers/001_GPIO_IP_OP/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_exti.h \
+ D:/BITSILICA/SALMAN/Learning/STM32/STM32_Projects/STM32L476RG_Drivers/001_GPIO_IP_OP/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_flash.h \
+ D:/BITSILICA/SALMAN/Learning/STM32/STM32_Projects/STM32L476RG_Drivers/001_GPIO_IP_OP/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_flash_ex.h \
+ D:/BITSILICA/SALMAN/Learning/STM32/STM32_Projects/STM32L476RG_Drivers/001_GPIO_IP_OP/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_flash_ramfunc.h \
+ D:/BITSILICA/SALMAN/Learning/STM32/STM32_Projects/STM32L476RG_Drivers/001_GPIO_IP_OP/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_pwr.h \
+ D:/BITSILICA/SALMAN/Learning/STM32/STM32_Projects/STM32L476RG_Drivers/001_GPIO_IP_OP/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_pwr_ex.h \
+ D:/BITSILICA/SALMAN/Learning/STM32/STM32_Projects/STM32L476RG_Drivers/001_GPIO_IP_OP/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_uart.h \
+ D:/BITSILICA/SALMAN/Learning/STM32/STM32_Projects/STM32L476RG_Drivers/001_GPIO_IP_OP/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_uart_ex.h
+D:/BITSILICA/SALMAN/Learning/STM32/STM32_Projects/STM32L476RG_Drivers/001_GPIO_IP_OP/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal.h:
+../Inc/stm32l4xx_hal_conf.h:
+D:/BITSILICA/SALMAN/Learning/STM32/STM32_Projects/STM32L476RG_Drivers/001_GPIO_IP_OP/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_rcc.h:
+D:/BITSILICA/SALMAN/Learning/STM32/STM32_Projects/STM32L476RG_Drivers/001_GPIO_IP_OP/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_def.h:
+D:/BITSILICA/SALMAN/Learning/STM32/STM32_Projects/STM32L476RG_Drivers/001_GPIO_IP_OP/Drivers/CMSIS/Device/ST/STM32L4xx/Include/stm32l4xx.h:
+D:/BITSILICA/SALMAN/Learning/STM32/STM32_Projects/STM32L476RG_Drivers/001_GPIO_IP_OP/Drivers/CMSIS/Device/ST/STM32L4xx/Include/stm32l476xx.h:
+D:/BITSILICA/SALMAN/Learning/STM32/STM32_Projects/STM32L476RG_Drivers/001_GPIO_IP_OP/Drivers/CMSIS/Include/core_cm4.h:
+D:/BITSILICA/SALMAN/Learning/STM32/STM32_Projects/STM32L476RG_Drivers/001_GPIO_IP_OP/Drivers/CMSIS/Include/cmsis_version.h:
+D:/BITSILICA/SALMAN/Learning/STM32/STM32_Projects/STM32L476RG_Drivers/001_GPIO_IP_OP/Drivers/CMSIS/Include/cmsis_compiler.h:
+D:/BITSILICA/SALMAN/Learning/STM32/STM32_Projects/STM32L476RG_Drivers/001_GPIO_IP_OP/Drivers/CMSIS/Include/cmsis_gcc.h:
+D:/BITSILICA/SALMAN/Learning/STM32/STM32_Projects/STM32L476RG_Drivers/001_GPIO_IP_OP/Drivers/CMSIS/Include/mpu_armv7.h:
+D:/BITSILICA/SALMAN/Learning/STM32/STM32_Projects/STM32L476RG_Drivers/001_GPIO_IP_OP/Drivers/CMSIS/Device/ST/STM32L4xx/Include/system_stm32l4xx.h:
+D:/BITSILICA/SALMAN/Learning/STM32/STM32_Projects/STM32L476RG_Drivers/001_GPIO_IP_OP/Drivers/STM32L4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+D:/BITSILICA/SALMAN/Learning/STM32/STM32_Projects/STM32L476RG_Drivers/001_GPIO_IP_OP/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_rcc_ex.h:
+D:/BITSILICA/SALMAN/Learning/STM32/STM32_Projects/STM32L476RG_Drivers/001_GPIO_IP_OP/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_gpio.h:
+D:/BITSILICA/SALMAN/Learning/STM32/STM32_Projects/STM32L476RG_Drivers/001_GPIO_IP_OP/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_gpio_ex.h:
+D:/BITSILICA/SALMAN/Learning/STM32/STM32_Projects/STM32L476RG_Drivers/001_GPIO_IP_OP/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_dma.h:
+D:/BITSILICA/SALMAN/Learning/STM32/STM32_Projects/STM32L476RG_Drivers/001_GPIO_IP_OP/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_cortex.h:
+D:/BITSILICA/SALMAN/Learning/STM32/STM32_Projects/STM32L476RG_Drivers/001_GPIO_IP_OP/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_exti.h:
+D:/BITSILICA/SALMAN/Learning/STM32/STM32_Projects/STM32L476RG_Drivers/001_GPIO_IP_OP/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_flash.h:
+D:/BITSILICA/SALMAN/Learning/STM32/STM32_Projects/STM32L476RG_Drivers/001_GPIO_IP_OP/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_flash_ex.h:
+D:/BITSILICA/SALMAN/Learning/STM32/STM32_Projects/STM32L476RG_Drivers/001_GPIO_IP_OP/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_flash_ramfunc.h:
+D:/BITSILICA/SALMAN/Learning/STM32/STM32_Projects/STM32L476RG_Drivers/001_GPIO_IP_OP/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_pwr.h:
+D:/BITSILICA/SALMAN/Learning/STM32/STM32_Projects/STM32L476RG_Drivers/001_GPIO_IP_OP/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_pwr_ex.h:
+D:/BITSILICA/SALMAN/Learning/STM32/STM32_Projects/STM32L476RG_Drivers/001_GPIO_IP_OP/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_uart.h:
+D:/BITSILICA/SALMAN/Learning/STM32/STM32_Projects/STM32L476RG_Drivers/001_GPIO_IP_OP/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_uart_ex.h:
---
+++ 002_UART_Tx_Polling/Debug/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_exti.d
@@ -0,0 +1,58 @@
+Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_exti.o: \
+ ../Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_exti.c \
+ D:/BITSILICA/SALMAN/Learning/STM32/STM32_Projects/STM32L476RG_Drivers/002_UART_Tx_Polling/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal.h \
+ ../Inc/stm32l4xx_hal_conf.h \
+ D:/BITSILICA/SALMAN/Learning/STM32/STM32_Projects/STM32L476RG_Drivers/002_UART_Tx_Polling/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_rcc.h \
+ D:/BITSILICA/SALMAN/Learning/STM32/STM32_Projects/STM32L476RG_Drivers/002_UART_Tx_Polling/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_def.h \
+ D:/BITSILICA/SALMAN/Learning/STM32/STM32_Projects/STM32L476RG_Drivers/002_UART_Tx_Polling/Drivers/CMSIS/Device/ST/STM32L4xx/Include/stm32l4xx.h \
+ D:/BITSILICA/SALMAN/Learning/STM32/STM32_Projects/STM32L476RG_Drivers/002_UART_Tx_Polling/Drivers/CMSIS/Device/ST/STM32L4xx/Include/stm32l476xx.h \
+ D:/BITSILICA/SALMAN/Learning/STM32/STM32_Projects/STM32L476RG_Drivers/002_UART_Tx_Polling/Drivers/CMSIS/Include/core_cm4.h \
+ D:/BITSILICA/SALMAN/Learning/STM32/STM32_Projects/STM32L476RG_Drivers/002_UART_Tx_Polling/Drivers/CMSIS/Include/cmsis_version.h \
+ D:/BITSILICA/SALMAN/Learning/STM32/STM32_Projects/STM32L476RG_Drivers/002_UART_Tx_Polling/Drivers/CMSIS/Include/cmsis_compiler.h \
+ D:/BITSILICA/SALMAN/Learning/STM32/STM32_Projects/STM32L476RG_Drivers/002_UART_Tx_Polling/Drivers/CMSIS/Include/cmsis_gcc.h \
+ D:/BITSILICA/SALMAN/Learning/STM32/STM32_Projects/STM32L476RG_Drivers/002_UART_Tx_Polling/Drivers/CMSIS/Include/mpu_armv7.h \
+ D:/BITSILICA/SALMAN/Learning/STM32/STM32_Projects/STM32L476RG_Drivers/002_UART_Tx_Polling/Drivers/CMSIS/Device/ST/STM32L4xx/Include/system_stm32l4xx.h \
+ D:/BITSILICA/SALMAN/Learning/STM32/STM32_Projects/STM32L476RG_Drivers/002_UART_Tx_Polling/Drivers/STM32L4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ D:/BITSILICA/SALMAN/Learning/STM32/STM32_Projects/STM32L476RG_Drivers/002_UART_Tx_Polling/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_rcc_ex.h \
+ D:/BITSILICA/SALMAN/Learning/STM32/STM32_Projects/STM32L476RG_Drivers/002_UART_Tx_Polling/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_gpio.h \
+ D:/BITSILICA/SALMAN/Learning/STM32/STM32_Projects/STM32L476RG_Drivers/002_UART_Tx_Polling/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_gpio_ex.h \
+ D:/BITSILICA/SALMAN/Learning/STM32/STM32_Projects/STM32L476RG_Drivers/002_UART_Tx_Polling/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_dma.h \
+ D:/BITSILICA/SALMAN/Learning/STM32/STM32_Projects/STM32L476RG_Drivers/002_UART_Tx_Polling/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_cortex.h \
+ D:/BITSILICA/SALMAN/Learning/STM32/STM32_Projects/STM32L476RG_Drivers/002_UART_Tx_Polling/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_exti.h \
+ D:/BITSILICA/SALMAN/Learning/STM32/STM32_Projects/STM32L476RG_Drivers/002_UART_Tx_Polling/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_flash.h \
+ D:/BITSILICA/SALMAN/Learning/STM32/STM32_Projects/STM32L476RG_Drivers/002_UART_Tx_Polling/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_flash_ex.h \
+ D:/BITSILICA/SALMAN/Learning/STM32/STM32_Projects/STM32L476RG_Drivers/002_UART_Tx_Polling/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_flash_ramfunc.h \
+ D:/BITSILICA/SALMAN/Learning/STM32/STM32_Projects/STM32L476RG_Drivers/002_UART_Tx_Polling/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_pwr.h \
+ D:/BITSILICA/SALMAN/Learning/STM32/STM32_Projects/STM32L476RG_Drivers/002_UART_Tx_Polling/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_pwr_ex.h \
+ D:/BITSILICA/SALMAN/Learning/STM32/STM32_Projects/STM32L476RG_Drivers/002_UART_Tx_Polling/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_uart.h \
+ D:/BITSILICA/SALMAN/Learning/STM32/STM32_Projects/STM32L476RG_Drivers/002_UART_Tx_Polling/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_uart_ex.h \
+ D:/BITSILICA/SALMAN/Learning/STM32/STM32_Projects/STM32L476RG_Drivers/002_UART_Tx_Polling/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_usart.h \
+ D:/BITSILICA/SALMAN/Learning/STM32/STM32_Projects/STM32L476RG_Drivers/002_UART_Tx_Polling/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_usart_ex.h
+D:/BITSILICA/SALMAN/Learning/STM32/STM32_Projects/STM32L476RG_Drivers/002_UART_Tx_Polling/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal.h:
+../Inc/stm32l4xx_hal_conf.h:
+D:/BITSILICA/SALMAN/Learning/STM32/STM32_Projects/STM32L476RG_Drivers/002_UART_Tx_Polling/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_rcc.h:
+D:/BITSILICA/SALMAN/Learning/STM32/STM32_Projects/STM32L476RG_Drivers/002_UART_Tx_Polling/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_def.h:
+D:/BITSILICA/SALMAN/Learning/STM32/STM32_Projects/STM32L476RG_Drivers/002_UART_Tx_Polling/Drivers/CMSIS/Device/ST/STM32L4xx/Include/stm32l4xx.h:
+D:/BITSILICA/SALMAN/Learning/STM32/STM32_Projects/STM32L476RG_Drivers/002_UART_Tx_Polling/Drivers/CMSIS/Device/ST/STM32L4xx/Include/stm32l476xx.h:
+D:/BITSILICA/SALMAN/Learning/STM32/STM32_Projects/STM32L476RG_Drivers/002_UART_Tx_Polling/Drivers/CMSIS/Include/core_cm4.h:
+D:/BITSILICA/SALMAN/Learning/STM32/STM32_Projects/STM32L476RG_Drivers/002_UART_Tx_Polling/Drivers/CMSIS/Include/cmsis_version.h:
+D:/BITSILICA/SALMAN/Learning/STM32/STM32_Projects/STM32L476RG_Drivers/002_UART_Tx_Polling/Drivers/CMSIS/Include/cmsis_compiler.h:
+D:/BITSILICA/SALMAN/Learning/STM32/STM32_Projects/STM32L476RG_Drivers/002_UART_Tx_Polling/Drivers/CMSIS/Include/cmsis_gcc.h:
+D:/BITSILICA/SALMAN/Learning/STM32/STM32_Projects/STM32L476RG_Drivers/002_UART_Tx_Polling/Drivers/CMSIS/Include/mpu_armv7.h:
+D:/BITSILICA/SALMAN/Learning/STM32/STM32_Projects/STM32L476RG_Drivers/002_UART_Tx_Polling/Drivers/CMSIS/Device/ST/STM32L4xx/Include/system_stm32l4xx.h:
+D:/BITSILICA/SALMAN/Learning/STM32/STM32_Projects/STM32L476RG_Drivers/002_UART_Tx_Polling/Drivers/STM32L4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+D:/BITSILICA/SALMAN/Learning/STM32/STM32_Projects/STM32L476RG_Drivers/002_UART_Tx_Polling/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_rcc_ex.h:
+D:/BITSILICA/SALMAN/Learning/STM32/STM32_Projects/STM32L476RG_Drivers/002_UART_Tx_Polling/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_gpio.h:
+D:/BITSILICA/SALMAN/Learning/STM32/STM32_Projects/STM32L476RG_Drivers/002_UART_Tx_Polling/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_gpio_ex.h:
+D:/BITSILICA/SALMAN/Learning/STM32/STM32_Projects/STM32L476RG_Drivers/002_UART_Tx_Polling/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_dma.h:
+D:/BITSILICA/SALMAN/Learning/STM32/STM32_Projects/STM32L476RG_Drivers/002_UART_Tx_Polling/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_cortex.h:
+D:/BITSILICA/SALMAN/Learning/STM32/STM32_Projects/STM32L476RG_Drivers/002_UART_Tx_Polling/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_exti.h:
+D:/BITSILICA/SALMAN/Learning/STM32/STM32_Projects/STM32L476RG_Drivers/002_UART_Tx_Polling/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_flash.h:
+D:/BITSILICA/SALMAN/Learning/STM32/STM32_Projects/STM32L476RG_Drivers/002_UART_Tx_Polling/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_flash_ex.h:
+D:/BITSILICA/SALMAN/Learning/STM32/STM32_Projects/STM32L476RG_Drivers/002_UART_Tx_Polling/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_flash_ramfunc.h:
+D:/BITSILICA/SALMAN/Learning/STM32/STM32_Projects/STM32L476RG_Drivers/002_UART_Tx_Polling/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_pwr.h:
+D:/BITSILICA/SALMAN/Learning/STM32/STM32_Projects/STM32L476RG_Drivers/002_UART_Tx_Polling/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_pwr_ex.h:
+D:/BITSILICA/SALMAN/Learning/STM32/STM32_Projects/STM32L476RG_Drivers/002_UART_Tx_Polling/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_uart.h:
+D:/BITSILICA/SALMAN/Learning/STM32/STM32_Projects/STM32L476RG_Drivers/002_UART_Tx_Polling/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_uart_ex.h:
+D:/BITSILICA/SALMAN/Learning/STM32/STM32_Projects/STM32L476RG_Drivers/002_UART_Tx_Polling/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_usart.h:
+D:/BITSILICA/SALMAN/Learning/STM32/STM32_Projects/STM32L476RG_Drivers/002_UART_Tx_Polling/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_usart_ex.h:
