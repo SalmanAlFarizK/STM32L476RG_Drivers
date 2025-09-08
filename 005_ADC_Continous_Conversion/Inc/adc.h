eb4d8c1150d80519de554cd16cf9b8dfe8ecfa10
SalmanAlFarizK <alfarizksalman@gmail.com>
2025-09-08 18:00:24 +0530

    added adc driver

---
+++ 005_ADC_Continous_Conversion/Inc/adc.h
@@ -0,0 +1,10 @@
+#ifndef ADC_H_
+#define ADC_H_
+#include <stdint.h>
+
+
+uint16_t GetADC1Channel1Val(void);
+void ADC1Start(void);
+
+
+#endif /* ADC_H_ */
---
+++ 005_ADC_Continous_Conversion/Src/adc.c
@@ -0,0 +1,82 @@
+#include "stm32l4xx_hal.h"
+#include <stdint.h>
+#include "adc.h"
+
+
+
+static void ADC_PC0_ContinousConversionInit(void);
+static HAL_StatusTypeDef ADC_WaitForEOC(uint32_t timeout);
+
+ADC_HandleTypeDef tAdc1;
+
+
+static void ADC_PC0_ContinousConversionInit(void)
+{
+	//Configure PC0 as analog pin.
+	GPIO_InitTypeDef tAdcGpio = {0};
+	ADC_ChannelConfTypeDef tAdcChanConf = {0};
+
+	__HAL_RCC_GPIOC_CLK_ENABLE();
+
+	tAdcGpio.Pin = GPIO_PIN_0;
+	tAdcGpio.Mode = GPIO_MODE_ANALOG;
+	tAdcGpio.Pull = GPIO_NOPULL;
+
+	HAL_GPIO_Init(GPIOC, &tAdcGpio);
+
+
+	//Configure ADc module for continuous conversion.
+	__HAL_RCC_ADC_CLK_ENABLE();
+	tAdc1.Instance = ADC1;
+	tAdc1.Init.ClockPrescaler  = ADC_CLOCK_SYNC_PCLK_DIV2;
+	tAdc1.Init.Resolution = ADC_RESOLUTION_12B;
+	tAdc1.Init.ContinuousConvMode = ENABLE;
+	tAdc1.Init.DiscontinuousConvMode = DISABLE;
+	tAdc1.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_NONE;
+	tAdc1.Init.ExternalTrigConv = ADC_SOFTWARE_START;
+	tAdc1.Init.DataAlign = ADC_DATAALIGN_RIGHT;
+	tAdc1.Init.NbrOfConversion = 1;
+	tAdc1.Init.DMAContinuousRequests = DISABLE;
+	tAdc1.Init.EOCSelection = ADC_EOC_SINGLE_CONV;
+
+	HAL_ADC_Init(&tAdc1);
+
+	tAdcChanConf.Channel = ADC_CHANNEL_1;
+	tAdcChanConf.Rank = 1;
+	tAdcChanConf.SamplingTime = ADC_SAMPLETIME_12CYCLES_5;
+
+	HAL_ADC_ConfigChannel(&tAdc1, &tAdcChanConf);
+
+	return;
+}
+
+uint16_t GetADC1Channel1Val(void)
+{
+	uint32_t uiAdcval = 0;
+
+	if (ADC_WaitForEOC(10) == HAL_OK)
+	{
+		uiAdcval = HAL_ADC_GetValue(&tAdc1);
+	}
+	else
+	{
+		// Handle timeout or error (return 0 or set error flag)
+		return 0;
+	}
+
+	return ((uint16_t)uiAdcval);
+}
+
+void ADC1Start(void)
+{
+	ADC_PC0_ContinousConversionInit();
+
+	HAL_ADC_Start(&tAdc1);
+
+	return;
+}
+
+static HAL_StatusTypeDef ADC_WaitForEOC(uint32_t timeout)
+{
+    return HAL_ADC_PollForConversion(&tAdc1, timeout);
+}
