4b3065c0eda14ccd853c59409c9ebcd28b17a35f
SalmanAlFarizK <alfarizksalman@gmail.com>
2025-09-09 14:23:28 +0530

    ADC DMA iproject is added

---
+++ 012_ADC_DMA/Inc/adc.h
@@ -0,0 +1,15 @@
+#ifndef ADC_H_
+#define ADC_H_
+#include <stdint.h>
+
+
+uint16_t GetADC1Channel1Val(void);
+void ADC1Start(void);
+void ADC_PC0_SingleConversionInit(void);
+void ADC_PC0_SingleConversionInitIt(void);
+void ADC1StartIt(void);
+void ADC_PC0_DMA_Init(void);
+void TestAdcDMA(void);
+
+
+#endif /* ADC_H_ */
